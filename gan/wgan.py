6910623c75dcf1b659f8b2bf3b658dea822c189d
CFHH <13436361@qq.com>
2022-04-22 16:38:31 +0800

    修改

--- gan/wgan.py
+++ gan/wgan.py
@@ -2,13 +2,13 @@ from __future__ import print_function, division
 
 import tensorflow as tf
 import numpy as np
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Sequential, Model
+from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from tensorflow.keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import UpSampling2D, Conv2D
+from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.optimizers import RMSprop
-import keras.backend as K
+import tensorflow.keras.backend as K
 
 import inputs
 import os
@@ -17,7 +17,7 @@ class WGAN():
     def __init__(self):
         # 输入参数
         self.rows = 120    # 动作的帧数 120，120/4 = 30
-        self.columns = 225 # 动作的特征数 225->220（24*9+3+1），220/4 = 55
+        self.columns = 220 # 动作的特征数 225->220（24*9+3+1），220/4 = 55
         self.channels = 1
         self.img_shape = (self.rows, self.columns, self.channels)
         self.latent_dim = 100
@@ -54,29 +54,29 @@ class WGAN():
         model = Sequential()
         # (None, 100) 输入
         model.add(Dense(128 * 30 * 55, activation="relu", input_dim=self.latent_dim))
-        # (None, 6272) 参数633472
+        # (None, 211200) 参数21331200
         model.add(Reshape((30, 55, 128)))
-        # (None, 7, 7, 128)  ——————>(rows/4, columns/4, 128)
+        # (None, 30, 55, 128)
         model.add(UpSampling2D())
-        # (None, 14, 14, 128)  ——————>(rows/2, columns/2, 128)
+        # (None, 60, 110, 128)
         model.add(Conv2D(128, kernel_size=4, padding="same"))
-        # (None, 14, 14, 128) 参数262272
+        # (None, 60, 110, 128) 参数262272
         model.add(BatchNormalization(momentum=0.8))
-        # (None, 14, 14, 128) 参数512
+        # (None, 60, 110, 128) 参数512
         model.add(Activation("relu"))
-        # (None, 14, 14, 128)
+        # (None, 60, 110, 128)
         model.add(UpSampling2D())
-        # (None, 28, 28, 128)  ——————>(rows, columns, 128)
+        # (None, 120, 220, 128)
         model.add(Conv2D(64, kernel_size=4, padding="same"))
-        # (None, 28, 28, 64) 参数131136  ——————>(rows, columns, 64)
+        # (None, 120, 220, 64) 参数131136
         model.add(BatchNormalization(momentum=0.8))
-        # (None, 28, 28, 64) 参数256
+        # (None, 120, 220, 64) 参数256
         model.add(Activation("relu"))
-        # (None, 28, 28, 64)
+        # (None, 120, 220, 64)
         model.add(Conv2D(self.channels, kernel_size=4, padding="same"))
-        # (None, 28, 28, 1) 参数1025  ——————>(rows, columns, channels)
+        # (None, 120, 220, 1) 参数1025
         model.add(Activation("tanh"))
-        # (None, 28, 28, 1) 输出
+        # (None, 120, 220, 1) 输出
 
         model.summary()
 
@@ -86,43 +86,43 @@ class WGAN():
 
     def build_critic(self):
         model = Sequential()
-        # (None, 28, 28, 1) 输入——————>(rows, columns, channels)
+        # (None, 120, 220, 1) 输入
         model.add(Conv2D(16, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
-        # (None, 14, 14, 16) 参数160——————>(rows/2, columns/2, 16)
+        # (None, 60, 110, 16) 参数160
         model.add(LeakyReLU(alpha=0.2))
-        # (None, 14, 14, 16)
+        # (None, 60, 110, 16)
         model.add(Dropout(0.25))
-        # (None, 14, 14, 16)
+        # (None, 60, 110, 16)
         model.add(Conv2D(32, kernel_size=3, strides=2, padding="same"))
-        # (None, 7, 7, 32) 参数4640——————>(rows/4 = 30, columns/4 = 55, 32)
+        # (None, 30, 55, 32) 参数4640
         model.add(ZeroPadding2D(padding=((0,0),(0,1))))
-        # (None, 8, 8, 32)——————>(rows/4 = 30, columns/4 + 1 = 56, 32)
+        # (None, 30, 56, 32)
         model.add(BatchNormalization(momentum=0.8))
-        # (None, 8, 8, 32) 参数128
+        # (None, 30, 56, 32) 参数128
         model.add(LeakyReLU(alpha=0.2))
-        # (None, 8, 8, 32)
+        # (None, 30, 56, 32)
         model.add(Dropout(0.25))
-        # (None, 8, 8, 32)
+        # (None, 30, 56, 32)
         model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
-        # (None, 4, 4, 64) 参数18496——————>(15, 28, 64)
+        # (None, 15, 28, 64) 参数18496
         model.add(BatchNormalization(momentum=0.8))
-        # (None, 4, 4, 64) 参数256
+        # (None, 15, 28, 64) 参数256
         model.add(LeakyReLU(alpha=0.2))
-        # (None, 4, 4, 64)
+        # (None, 15, 28, 64)
         model.add(Dropout(0.25))
-        # (None, 4, 4, 64)
+        # (None, 15, 28, 64)
         model.add(Conv2D(128, kernel_size=3, strides=1, padding="same"))
-        # (None, 4, 4, 128) 参数73856——————>(15, 28, 128)
+        # (None, 15, 28, 128) 参数73856
         model.add(BatchNormalization(momentum=0.8))
-        # (None, 4, 4, 128) 参数512
+        # (None, 15, 28, 128) 参数512
         model.add(LeakyReLU(alpha=0.2))
-        # (None, 4, 4, 128)
+        # (None, 15, 28, 128)
         model.add(Dropout(0.25))
-        # (None, 4, 4, 128)
+        # (None, 15, 28, 128)
         model.add(Flatten())
-        # (None, 2048)
+        # (None, 53760)
         model.add(Dense(1))
-        # (None, 1) 参数2049
+        # (None, 1) 参数53761
         # 输出：-1表示valid也就是数据集的真图，1是fake也就是生成的假图
 
         model.summary()
@@ -173,6 +173,7 @@ class WGAN():
             #  Sample
             # ---------------------
             if epoch % sample_interval == 0:
+                self.save_model(epoch)
                 self.sample_motions(epoch)
 
     def sample_motions(self, epoch):
@@ -182,11 +183,21 @@ class WGAN():
         gen_motions = self.generator.predict(noise)
 
         for i in range(gen_num):
-          gen_motion = gen_motions[i].numpy()
+          gen_motion = gen_motions[i]
           save_path = os.path.join(output_dir, "%04d_%02d.npy" % (epoch, i))
           np.save(save_path, gen_motion)
+
+    def save_model(self, epoch):
+        def save(model, epoch, model_name):
+            model_path = "./saved_model/%d_%s.json" % (epoch, model_name)
+            weights_path = "./saved_model/%d_%s_weights.hdf5" % (epoch, model_name)
+            json_string = model.to_json()
+            open(model_path, 'w').write(json_string)
+            model.save_weights(weights_path)
+        save(self.generator, epoch, "generator")
+        save(self.critic, epoch, "critic")
 
 
 if __name__ == '__main__':
     wgan = WGAN()
-    wgan.train(epochs=100000, batch_size=32, sample_interval=1000)
+    wgan.train(epochs=10000, batch_size=32, sample_interval=500)
